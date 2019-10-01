b7ec0b896c07b48545afeed6f73fdbc2d1b91171
moelsayed <m.elsayed@gmail.com>
2019-10-01 06:58:06 +0200

    wip

--- vendor/github.com/rancher/kontainer-engine/drivers/rke/rke_driver.go
+++ vendor/github.com/rancher/kontainer-engine/drivers/rke/rke_driver.go
@@ -222,7 +222,7 @@ func (d *Driver) Update(ctx context.Context, clusterInfo *types.ClusterInfo, opt
 	if err != nil {
 		return nil, err
 	}
-	logrus.Infof("melsayed------------------------------------------------ in DRIVER UPDATE %v", rkeConfig.Services.KubeAPI.SecretsEncryptionConfig.CustomConfig)
+	logrus.Infof("melsayed------------------------------------------------ in DRIVER UPDATE %v", rkeConfig.Services.KubeAPI.SecretsEncryptionConfig.CustomConfig.DeepCopy())
 	dialers, externalFlags := d.getFlags(rkeConfig, stateDir)
 	if err := cmd.ClusterInit(ctx, &rkeConfig, dialers, externalFlags); err != nil {
 		return nil, err
